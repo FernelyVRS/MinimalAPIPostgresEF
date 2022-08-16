05e4ce666895d5bc4d97cf17257c25c3ec280073
FernelyVRS <fernelyveras.com>
2022-08-16 16:57:41 -0400

    fist commit

---
+++ Data/OfficeDB.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MinimalAPI_PostgresEF.Models;
+
+namespace MinimalAPI_PostgresEF.Data
+{
+    public class OfficeDB : DbContext
+    {
+
+        public OfficeDB(DbContextOptions<OfficeDB> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Employee> Employees => Set<Employee>();
+    }
+}
---
+++ Models/Employee.cs
@@ -0,0 +1,16 @@
+﻿namespace MinimalAPI_PostgresEF.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        
+        public string FirstName { get; set; }
+        
+        public string LastName { get; set; }
+        
+        public string Branch { get; set; }
+        
+        public int Age { get; set; }
+
+    }
+}
